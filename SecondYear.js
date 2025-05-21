a140697fd8d9da29c65ac418056b3f1f5ac119dd
kartikeya7609 <kartikeyakk2007@gmail.com>
2025-05-21 14:08:01 +0530

    commit all second year Members

---
+++ SecondYear.js
@@ -0,0 +1,152 @@
+export const secondYear = [
+  {
+    image: "Second-Year-IEEE-Members-Images/K.M.S Kartikeya.png",
+    fullName: "K.M.S Kartikeya",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/meher-sri-kartikeya-kotha-135997217?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle: "https://www.instagram.com/k_m_s_k_7/",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Shivam kumar.jpg",
+    fullName: "Shivam Kumar",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/shivamkumarnit?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/shivam.nitd?igsh=dWV1c2M0MGF0cjNp",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Govind kumar.jpg",
+    fullName: "Govind Kumar",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/govind-kumar-80a305348?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/samael_1407?igsh=OG9rMjVlOWI3ODl0",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Manish Kumar Sinha.jpg",
+    fullName: "Manish Kumar Sinha",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/manish-kumar-sinha-5ba546318?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/manishksinha23?igsh=MTV2ejU2bWdlNHN3MA==",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Abhijit Paul.jpg",
+    fullName: "Abhijit Paul",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/abhijit-paul-a05218314?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/rousbk11sans?igsh=MXZsbXUxbWllZmZyeQ==",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Pradip Pal.jpg",
+    fullName: "Pradip Pal",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/pradip-pal-aa4a95332?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/pradip_1729?igsh=cGNjOW9ianA4YXg1",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Pennada Karthik.jpg",
+    fullName: "Pennada Karthik",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/karthik-pennada-a38369344?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/unanimous_p.k_0607?utm_source=qr&igsh=YzFvYTRjeDhsOXZ2",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Penumatsa Raju.jpg",
+    fullName: "Penumatsa Raju",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/penumatsa-v-s-a-raju-0b5634337?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/ashketchupisbest?igsh=dXN1NWRkenJ4eWkw",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Gunta Poojitha.jpg",
+    fullName: "Gunta Poojitha",
+    post: "Member",
+    linkedIn: "https://www.linkedin.com/in/poojitha-gunta-5bb950326",
+    instagramHandle:
+      "https://www.instagram.com/poojitha_327_?igsh=c2VmZXNpc3Z1Y3E4",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Thanmai Sree.jpg",
+    fullName: "Thanmai Sree",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/thanmai-sree-bojja-022653327?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/thanmai_sree_bojja?igsh=dm03bmQ1c2w0NDc4",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Bhavith Mukundh .jpg",
+    fullName: "Bhavith Mukundh",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/bhavith-mukundh-b93934366?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/bhavithmukundh.p?igsh=MW9lOGR1OG1pamFrdg==",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Rudrayan Pramanick.jpg",
+    fullName: "Rudrayan Pramanick",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/rudrayan-pramanick-040647311?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/rudrayan_17?igsh=MTFtbXhsc2Z4dTR4eQ==",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Ajitesh Shukla.jpg",
+    fullName: "AJITESH SHUKLA",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/ajitesh-shukla-b02130346?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/ajitesh_s?igsh=NTk5ZWRremo5dnFp",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Shyam Siddarath.jpg",
+    fullName: "Shyam Siddharth",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/tangirala-shyam-siddharth-b529aa329?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/siddu_1090?igsh=dzJoNGJtY3Q1b3Bk",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Indranil Dhar.jpg",
+    fullName: "Indranil Dhar",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/indranil-dhar-123528321?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagramHandle:
+      "https://www.instagram.com/indranil9103?igsh=dHpsY2M4cGVpbXc3",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/D.Varsha.jpg",
+    fullName: "D. Varsha",
+    post: "Member",
+    linkedIn:
+      "https://www.linkedin.com/in/varsha-dushpakula-067a44366?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    instagramHandle:
+      "https://www.instagram.com/di_mple.22?igsh=MWs5ZjBoejhkZDNpeQ%3D%3D&utm_source=qr",
+  },
+  {
+    image: "Second-Year-IEEE-Members-Images/Hanumanthu Harshan.jpg",
+    fullName: "Hanumanthu Harshan",
+    post: "Member",
+    linkedIn: "https://www.linkedin.com/in/",
+    instagramHandle:
+      "https://www.instagram.com/hanumanthharshan35?igsh=bWFlZ2RsdW0yMnh2",
+  },
+];
